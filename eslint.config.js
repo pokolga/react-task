3c7a5278ca5806e9b42ccc2bc425d745d323c939
pokolga <pokolga1801@gmail.com>
2025-07-09 18:49:18 +0300

    refactor: apply eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,46 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ["dist", "node_modules"] },
+
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: "module",
       globals: globals.browser,
     },
-  },
-])
+
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strict,
+      eslintPluginPrettier,
+    ],
+
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs["recommended-latest"].rules,
+
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-console": "warn",
+      "no-debugger": "error",
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  }
+);
