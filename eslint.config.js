68b6094f009243ed51037b9e198d5b74b48a0ba8
pokolga <pokolga1801@gmail.com>
2025-07-11 20:51:06 +0300

    refactor: apply tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,50 @@
-import js from "@eslint/js";
-import globals from "globals";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import tseslint from "typescript-eslint";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules"] },
+  {
+    ignores: ['dist', 'node_modules'],
+  },
 
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      sourceType: "module",
+      sourceType: 'module',
       globals: globals.browser,
     },
-
     plugins: {
+      prettier: pluginPrettier,
       react,
-      "react-hooks": reactHooks,
+      'react-hooks': reactHooks,
     },
-
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strict,
-      eslintPluginPrettier,
-    ],
-
     rules: {
       ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
-      ...reactHooks.configs["recommended-latest"].rules,
-
-      "@typescript-eslint/no-explicit-any": "error",
-      "no-console": "warn",
-      "no-debugger": "error",
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs['recommended-latest'].rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-console': 'warn',
+      'no-debugger': 'error',
     },
-
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
+
+  // Добавляем Prettier как отдельный конфиг
+  prettier,
+
+  // Добавляем строгие правила TypeScript как отдельные объекты
+  ...tseslint.configs.strict,
+
+  // Добавляем базовые правила JS
+  js.configs.recommended
 );
