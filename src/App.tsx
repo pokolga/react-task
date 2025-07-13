fbcbbd51eaeffabdfbe9e6098adedf8f6b2136df
pokolga <pokolga1801@gmail.com>
2025-07-13 19:11:22 +0300

    feat: add ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Search from './components/search';
 import type { Character } from './models/types';
 import { getData } from './services/fetch';
 import { spinnerDelay } from './models/constants';
+import ErrorBoundary from './components/errorBoundary';
+import ErrorButton from './components/errorButton';
 
 interface AppState {
   results: Character[];
@@ -24,7 +26,11 @@ class App extends React.Component<unknown, AppState> {
       const data = await getData(query.trim());
       this.setState({ results: data });
     } catch (error) {
-      this.setState({ results: [], error: `${error} Nothing found for your request!` });
+      let errorMessage = '';
+      if (typeof error === 'string' && /404/.test(error)) {
+        errorMessage = 'Nothing was found for your request';
+      }
+      this.setState({ results: [], error: `${error} ${errorMessage}!` });
     } finally {
       setTimeout(() => {
         this.setState({ loading: false });
@@ -36,12 +42,18 @@ class App extends React.Component<unknown, AppState> {
     return (
       <>
         <h1 className="text-2xl font-bold text-blue-600 text-center my-4">Characters Rick&Morty</h1>
-        <Search onSearch={this.whenSearch} />
-        <Result
-          results={this.state.results}
-          error={this.state.error}
-          loading={this.state.loading}
-        />
+        <ErrorBoundary
+          fallback={<p className="text-2xl font-bold text-red">Something went wrong...</p>}
+        >
+          <Search onSearch={this.whenSearch} />
+
+          <Result
+            results={this.state.results}
+            error={this.state.error}
+            loading={this.state.loading}
+          />
+          <ErrorButton />
+        </ErrorBoundary>
       </>
     );
   }
