68ccec8b9fdf140e68eca68ed967ec6f266fad48
pokolga <pokolga1801@gmail.com>
2025-08-10 22:09:43 +0300

    fix: add staleTime to useResult

--- src/hooks/useResult.tsx
+++ src/hooks/useResult.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import { getData } from './fetch';
+import { getData } from '../services/fetch';
+import { defaultCasheTime } from '../models/constants';
 
 export function useResult(activeQuery: string, pageNum: number) {
   return useQuery({
@@ -9,7 +10,7 @@ export function useResult(activeQuery: string, pageNum: number) {
       return data;
     },
     retry: false,
-    staleTime: 0,
+    staleTime: defaultCasheTime,
     select: (data) => ({
       results: data.results,
       info: data.info,
